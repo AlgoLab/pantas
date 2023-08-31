b21ff1b9cfdebf2841ba51660b25e8e80b4ef767
Simone Ciccolella <simone.ciccolella@unimib.it>
2023-08-31 15:06:26 +0200

    Add scripts for GFA annotation with splicing junctions

---
+++ scripts/add_junctions.py
@@ -0,0 +1,80 @@
+import gfautils
+from Bio import SeqIO
+
+def debug_align(s1, s2):
+    print("Sequence len:", len(s1))
+    l1 = "path: "
+    l2 = "seq : "
+    m = min(len(s1), len(s2))
+    for i in range(m):
+        if s1[i] == s2[i]:
+            l1 += f"\033[1;32m{s1[i]}"
+            l2 += f"\033[1;32m{s2[i]}"
+        else:
+            l1 += f"\033[1;31m{s1[i]}"
+            l2 += f"\033[1;31m{s2[i]}"
+    if len(s1) > m:
+        for i in range(m, len(s1)):
+            l1 += f"\033[1;34m{s1[i]}"
+            l2 += "\033[1;34m-"
+    if len(s2) > m:
+        for i in range(m, len(s2)):
+            l1 += "\033[1;31m*"
+            l2 += f"\033[1;31m{s2[i]}"
+    l1 += f"\033[0m"
+    l2 += f"\033[0m"
+    print(l1)
+    print(l2)
+
+def main(args):
+    gfa = gfautils.GFA(args.GFA)
+
+    for record in SeqIO.parse(args.GFFW, "fasta"):
+        # print(record.name)
+        if args.debug:
+            debug_align(gfa.pseq(f'{record.name}_R1'), record.seq)
+        assert(gfa.pseq(f'{record.name}_R1') == record.seq)
+        
+        desc = record.description.split()
+        segs = desc[[desc.index(l) for l in desc if l.startswith("segs:")][0]].split(':')[1].split(',')
+        intsegs = []
+
+        for seg in segs:
+            s, e = seg.split('-')
+            s = int(s); e = int(e)
+            intsegs.append((s, e))
+
+        # print(intsegs)
+        curr_seg = 0
+        cum_len = 0
+        nid_prev=None
+        is_reverse = gfa.paths[f'{record.name}_R1'].is_reverse
+        for nid_curr in gfa.pnodes(f'{record.name}_R1'):
+            cum_len += gfa.nlen(nid_curr)
+            if cum_len == intsegs[curr_seg][1]:
+                curr_seg+=1
+                # print(nid_prev, nid_curr)
+                _lkey = [nid_prev, nid_curr] if not is_reverse else [nid_curr, nid_prev]
+                gfa.link(*_lkey).addjunction(f'{record.name}.{curr_seg}.{curr_seg+1}')
+            
+            gfa.nodes[nid_curr].addexon(f'{record.name}.{curr_seg+1}')
+
+            nid_prev=nid_curr
+        assert(curr_seg == len(intsegs))
+
+    gfa.print()
+
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description='Check that GFA Path are identical to input FAs')
+    parser.add_argument('GFA', type=str,
+                        help='Path to GFA')
+    parser.add_argument('GFFW', type=str,
+                        help='Path to gffread out')
+    parser.add_argument('--debug', action="store_true",
+                       help='Print alignment debug')
+    args = parser.parse_args()
+
+    main(args)
---
+++ scripts/gfautils.py
@@ -0,0 +1,143 @@
+from typing import Any
+
+
+def fa_complement(fa:str):
+    _fa = ''
+    for x in fa.lower():
+        if x == 'a':
+            _fa+='T'
+        elif x == 'c':
+            _fa+='G'
+        elif x == 'g':
+            _fa+='C'
+        elif x == 't':
+            _fa+='A'
+        else:
+            _fa+="N"
+    return _fa
+
+class _gfanode:
+    def __init__(self, nid:str, seq:str) -> None:
+        self.nid = nid
+        self.seq = seq
+        self.len = len(self.seq)
+        self.exons = []
+    
+    # def __getattr__(self, __name: str) -> Any:
+    #     return self.attrs[__name]
+    
+    # def __setattr__(self, __name: str, __value: Any) -> None:
+    #     self.attrs[__name] == __value
+    
+    def addexon(self, exon: str) -> None:
+        self.exons.append(exon)
+
+class _gfapath:
+    def __init__(self, pid:str, nodes:list[str], overlap:str, is_reverse:bool=False) -> None:
+        self.pid = pid
+        self.nodes = nodes
+        self.overlap = overlap
+
+        self.is_reverse = is_reverse
+    
+    def seq(self, nodes: list[_gfanode]):
+        _fa = ''
+        if not self.is_reverse:
+            for n in self.nodes:
+                _fa += nodes[n].seq
+        else:
+            for n in self.nodes:
+                _fa += fa_complement(nodes[n].seq[::-1])
+        return _fa
+    
+    def strnodes(self) -> str:
+        _joiner = '+,' if not self.is_reverse else '-,'
+        return _joiner.join(self.nodes) + _joiner[0]
+
+    
+class _gfalink:
+    def __init__(self, nid_from:str, orient_from:str, nid_to:str, orient_to:str, overlap:str) -> None:
+        self.nid_from = nid_from
+        self.orient_from = orient_from
+        self.nid_to = nid_to
+        self.orient_to = orient_to
+        self.overlap = overlap
+
+        self.junctions = []
+
+    def addjunction(self, transcript:str) -> None:
+        self.junctions.append(transcript)
+
+class GFA:
+    def __init__(self, filepath:str=None) -> None:
+        self.nodes = {}
+        self.paths = {}
+        self.links = {}
+
+        if filepath:
+            for line in open(filepath):
+                line = line.strip()
+                if line.startswith("S"):
+                    _, nid, seq = line.split()
+                    self.add_node(nid, seq)
+                elif line.startswith("P"):
+                    _, pid, p, overlap = line.split()
+                    assert not ("+," in p[:-1] and "-," in p[:-1])
+                    if "+," in p[:-1]:
+                        self.add_path(pid, p[:-1].split("+,"), overlap)
+                    else:
+                        self.add_path(pid, p[:-1].split("-,"), overlap, is_reverse=True)
+                elif line.startswith("L"):
+                    self.add_link(*line.split()[1:])
+
+    def add_node(self, nid:str, seq:str) -> None:
+        self.nodes[nid] = _gfanode(nid, seq)
+    
+    def add_path(self, pid:str, nodes:list[str], overlap:str, is_reverse:bool=False) -> None:
+        self.paths[pid] = _gfapath(pid, nodes, overlap, is_reverse)
+
+    def add_link(self, nid_from:str, orient_from:str, nid_to:str, orient_to:str, overlap:str) -> None:
+        self.links[(nid_from, nid_to)] = _gfalink(nid_from, orient_from, nid_to, orient_to, overlap)
+    
+    def link(self, nid_from:str, nid_to:str) -> _gfalink:
+        return self.links[(nid_from, nid_to)]
+
+    def pseq(self, pid:str) -> str:
+        return self.paths[pid].seq(self.nodes)
+    
+    def pnodes(self, pid:str) -> str:
+        for n in self.paths[pid].nodes:
+            yield n
+    
+    def nlen(self, nid:str) -> int:
+        return self.nodes[nid].len
+
+    def print(self) -> None:
+        for nid in self.nodes:
+            _node = self.nodes[nid]
+            print(
+                'S', nid, _node.seq, f'LN:i:{_node.len}', sep='\t', end=''
+            )
+            if len(_node.exons) > 0:
+                print(
+                    f'\tEX:Z:{",".join(_node.exons)}', end=''
+                )
+            print()
+
+        for lid in self.links:
+            _link = self.links[lid]
+            print(
+                'L', _link.nid_from, _link.orient_from, _link.nid_to, _link.orient_to, _link.overlap, sep='\t', end=''
+            )
+            if len(_link.junctions) > 0:
+                print(
+                    f'\tJN:Z:{",".join(_link.junctions)}', end=''
+                )
+            print()
+
+        for pid in self.paths:
+            _path = self.paths[pid]
+            print(
+                'P', _path.pid, _path.strnodes(), _path.overlap, sep='\t', end=''
+            )
+            print()

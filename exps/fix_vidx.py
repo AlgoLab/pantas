e194f2452b4a9acca8e757917e8044e498915f51
Luca Denti <ldenti92@gmail.com>
2023-09-07 19:06:31 +0200

    Refactor script for cleaning VCF ids

--- exps/fix_vidx.py
+++ exps/fix_vidx.py
@@ -1,14 +1,31 @@
 import sys
 import gzip
 
-i = 1
-print("Input file: " + sys.argv[1] + "\nOutput file: stdout", file=sys.stderr)
-with gzip.open(sys.argv[1], "rt") as fr:
-    for line in fr:
-        if line.startswith("#"):
-            print(line, end="")
-        else:
-            tokens = line.split()
-            tokens[2] = tokens[0] + "." + str(i)
-            print("\t".join(tokens))
-            i += 1
+from pysam import VariantFile
+
+
+def main():
+    in_vcf = sys.argv[1] if len(sys.argv) > 1 else sys.stdin
+    vcf = VariantFile(sys.stdin)
+
+    for line in str(vcf.header).split("\n"):
+        if line == "":
+            continue
+        if line.startswith("##contig="):
+            line = line.replace("chr", "")
+        print(line)
+
+    last_pos = -1
+    i = 1
+    for record in vcf:
+        if record.pos != last_pos:
+            i = 1
+        record.id = (
+            record.contig.replace("chr", "") + "-" + str(record.pos) + "." + str(i)
+        )
+        if record.contig.startswith("chr"):
+            print(str(record)[3:], end="")
+
+
+if __name__ == "__main__":
+    main()

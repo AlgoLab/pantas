6abd07e3e978568d04aba4f34e1247f8bfa47d7c
Luca Denti <ldenti@proton.me>
2024-07-23 11:52:29 +0200

    Add support for novel events to remap + Add filter on intron length

--- scripts/remap.py
+++ scripts/remap.py
@@ -1,5 +1,5 @@
-import sys
 import re
+
 
 def parse_region(r):
     if r == "?":
@@ -11,6 +11,13 @@ def parse_region(r):
     s, e = r.split(":")[1].split("-")
     return chrom, int(s), int(e), imprecise
 
+
+def get_region_size(r):
+    chrom = r.split(":")[0]
+    s, e = r.split(":")[1].split("-")
+    return int(e) - int(s)
+
+
 def get_reference_transcript(hts):
     t = [x for x in hts.split("|") if x.split("_")[-1][0] == "R"]
     if len(t) == 0:
@@ -18,13 +25,11 @@ def get_reference_transcript(hts):
     else:
         return t[0]
 
-def main():
-    quant_path = sys.argv[1]
-    gtf_path = sys.argv[2]
 
+def main(args):
     # Iterate over quant csv to get transcripts we need
     transcripts = {}
-    for line in open(quant_path):
+    for line in open(args.CSV):
         if line.startswith("etype"):
             continue
         junction1_names, junction2_names, junction3_names = line.strip("\n").split(",")[
@@ -41,7 +46,7 @@ def main():
             transcripts[t] = []
 
     # Parse GTF to get exon positions
-    for line in open(gtf_path):
+    for line in open(args.GTF):
         if line.startswith("#"):
             continue
         line = line.strip("\n").split("\t")
@@ -83,7 +88,7 @@ def main():
         sep=",",
     )
     # Reiterate over quant CSV to add reference positions
-    for line in open(quant_path):
+    for line in open(args.CSV):
         if line.startswith("etype"):
             continue
         (
@@ -129,7 +134,7 @@ def main():
                 s = transcripts[tidx][e - 1][0]
                 # end is end of exon
                 e = transcripts[tidx][e - 1][1]
-                assert s <= e # we can have introns of length 1
+                assert s <= e  # we can have introns of length 1
                 positions.append(f"{chrom}:{s}-{e}" + ("?" if imprecise else ""))
             else:
                 # Splicing junction
@@ -138,16 +143,42 @@ def main():
                 s = transcripts[tidx][e1 - 1][1] + 1
                 # end is start of second exon - 1
                 e = transcripts[tidx][e2 - 1][0] - 1
-                assert s <= e # we can have introns of length 1
+                assert s <= e  # we can have introns of length 1
                 positions.append(f"{chrom}:{s}-{e}" + ("?" if imprecise else ""))
 
-        # exon skippings are good. We want inclusion1, inclusion2, skipping junction
-        # alternative splice sites, we have to put the shorter junction first
-        # intron retention, we have only retained intron. So we put it first
-        if etype[0] == "A":
-            if positions[0] == "?":
-                positions[0] = positions[1]
-                positions[1] = "?"
+        if annotation_type == "annotated":
+            # exon skippings are good. We want inclusion1, inclusion2, skipping junction
+            # alternative splice sites, we have to put the shorter junction first
+            # intron retention, we have only retained intron. So we put it first
+            if etype[0] == "A":
+                if positions[0] == "?":
+                    positions[0] = positions[1]
+                    positions[1] = "?"
+                    junction1_name = junction2_name
+                    junction2_name = "?"
+                    positions[0] = positions[1]
+                    positions[1] = "?"
+                    junction1_nodes = junction2_nodes
+                    junction2_nodes = "."
+                    W1, W2 = W2, W1
+                    psi_c1, psi_c2 = psi_c2, psi_c1
+                    dpsi = 1.0 - float(dpsi)
+                elif positions[1] == "?":
+                    pass
+                else:
+                    p0 = parse_region(positions[0])
+                    p1 = parse_region(positions[1])
+                    if p0[2] - p0[1] > p1[2] - p1[1]:
+                        positions[0], positions[1] = positions[1], positions[0]
+                        junction1_name, junction2_name = junction2_name, junction1_name
+                        junction1_nodes, junction2_nodes = (
+                            junction2_nodes,
+                            junction1_nodes,
+                        )
+                        W1, W2 = W2, W1
+                        psi_c1, psi_c2 = psi_c2, psi_c1
+                        dpsi = 1.0 - float(dpsi)
+            elif etype == "IR":
                 junction1_name = junction2_name
                 junction2_name = "?"
                 positions[0] = positions[1]
@@ -156,37 +187,37 @@ def main():
                 junction2_nodes = "."
                 W1, W2 = W2, W1
                 psi_c1, psi_c2 = psi_c2, psi_c1
-                dpsi = 1.0-float(dpsi)
-            elif positions[1] == "?":
-                pass
-            else:
-                p0 = parse_region(positions[0])
-                p1 = parse_region(positions[1])
-                if p0[2] - p0[1] > p1[2] - p1[1]:
-                    positions[0], positions[1] = positions[1], positions[0]
-                    junction1_name, junction2_name = junction2_name, junction1_name
-                    junction1_nodes, junction2_nodes = junction2_nodes, junction1_nodes
-                    W1, W2 = W2, W1
-                    psi_c1, psi_c2 = psi_c2, psi_c1
-                    dpsi = 1.0-float(dpsi)
-        elif etype == "IR":
-            junction1_name = junction2_name
-            junction2_name = "?"
-            positions[0] = positions[1]
-            positions[1] = "?"
-            junction1_nodes = junction2_nodes
-            junction2_nodes = "."
-            W1, W2 = W2, W1
-            psi_c1, psi_c2 = psi_c2, psi_c1
-            dpsi = 1.0-float(dpsi)
+                dpsi = 1.0 - float(dpsi)
 
         htype = "reference"
-        if etype == "ES" and annotation_type == "annotated" and (positions[0] == "?" or positions[1] == "?" or positions[2] == "?"):
-            htype = "haplotype"
-        if etype[0] == "A" and annotation_type == "annotated" and (positions[0] == "?" or positions[1] == "?"):
-            htype = "haplotype"
-        if etype == "IR" and annotation_type == "annotated" and (positions[0] == "?"):
-            htype = "haplotype"
+        if annotation_type == "annotated":
+            if etype == "ES" and (
+                positions[0] == "?" or positions[1] == "?" or positions[2] == "?"
+            ):
+                htype = "haplotype"
+            if etype[0] == "A" and (positions[0] == "?" or positions[1] == "?"):
+                htype = "haplotype"
+            if etype == "IR" and positions[0] == "?":
+                htype = "haplotype"
+        else:
+            if etype == "ES" and (
+                positions[0] == "?" and positions[1] == "?" and positions[2] == "?"
+            ):
+                htype = "haplotype"
+            if etype[0] == "A" and (positions[0] == "?" and positions[1] == "?"):
+                htype = "haplotype"
+            if etype == "IR" and (positions[0] == "?" and positions[1] == "?"):
+                htype = "haplotype"
+
+        if htype == "reference":
+            if any(
+                [
+                    get_region_size(p) < args.min_intron_size
+                    for p in positions
+                    if p != "?"
+                ]
+            ):
+                continue
 
         print(
             etype,
@@ -214,4 +245,20 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        prog="Remap",
+        description="",
+    )
+    parser.add_argument("CSV", help="Quantification in CSV format")
+    parser.add_argument("GTF", help="Annotation in GTF format")
+    parser.add_argument(
+        "-i",
+        dest="min_intron_size",
+        help="Minimum intron size for a junction (default: 25)",
+        type=int,
+        default=25,
+    )
+    args = parser.parse_args()
+    main(args)
